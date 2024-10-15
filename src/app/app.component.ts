324e035bc426264218bf0d1ca4763bfa1693ceda
simplysekinah <akinolasekinat123@gmail.com>
2024-10-15 14:55:28 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SectionOneComponent } from "./section-one/section-one.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SectionTwoComponent } from "./section-two/section-two.component";
+import { SectionThreeComponent } from "./section-three/section-three.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SectionOneComponent, NavbarComponent, SectionTwoComponent, SectionThreeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
